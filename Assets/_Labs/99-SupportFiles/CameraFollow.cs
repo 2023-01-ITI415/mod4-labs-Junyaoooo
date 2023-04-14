b2f10a8eef3d41fee4d34d7b756694a54a789b06
Junyaoooo <123599025+Junyaoooo@users.noreply.github.com>
2023-04-13 20:17:30 -0400

    end

---
+++ Assets/_Labs/99-SupportFiles/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject m_Player;
+    public Vector3 offset;
+    private float _pointY;
+    public float damping = 1;
+    void Start()
+    {
+        offset = transform.position - m_Player.transform.position;
+    }
+
+    void Update()
+    {
+
+        _pointY = m_Player.transform.eulerAngles.y;
+        Quaternion rotatiom = Quaternion.Euler(0, _pointY, 0);
+        transform.position = Vector3.Lerp(transform.position, m_Player.transform.position + (rotatiom * offset), Time.deltaTime * damping);
+        transform.LookAt(m_Player.transform.position);
+
+    }
+    void LookAt()
+    {
+        var dir = (m_Player.transform.position - transform.position).normalized;
+        var quat = Quaternion.LookRotation(dir);
+        transform.rotation = quat;
+    }
+
+}
+
+
+
+
---
+++ Assets/_Labs/99-SupportFiles/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    //人物移动速度  
+    public int moveSpeed = 7;
+    //初始化人物位置  
+    public void Awake()
+    {
+        
+    }
+    // Use this for initialization  
+    void Start()
+    {
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Translate(Vector2.left*Time.deltaTime*moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+            
+    }
+
+  
+}
